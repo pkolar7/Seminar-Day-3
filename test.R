06fecad4e0ebea68419c4d9678be492666c0ccb0
Petra Kolar <petra.kolar@uzh.ch>
2023-02-08 10:48:34 +0100

    test

---
+++ test.R
@@ -0,0 +1,16 @@
+getwd()
+# R Seminar Day 3
+
+### Exercise Day 3 17 What is GIT
+# 1. Install Git on your computer: https://git-scm.com.
+# 2. Create a Github account: https://github.com.
+
+
+### Exercise Day 3 18 Let’s get started with Git
+
+# 1. Create an RSA key in RStudio and add it to your GitHub account.
+# 2. Create a new public repository on GitHub.
+# 3. Create a RStudio project with version control, that is connected to the new repository on GitHub.
+# 4. Create a test file, add some changes, and push it to your GitHub repository.
+# 5. Clone the following repository to your computer: https://github.com/octocat/Spoon-Knife
+
